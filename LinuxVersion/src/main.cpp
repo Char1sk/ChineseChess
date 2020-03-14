ccf669a5938367998fdf9a09961bf40e39682455
Char1sk <61937178+Char1sk@users.noreply.github.com>
2020-03-14 22:02:34 +0800

    Create main.cpp

---
+++ LinuxVersion/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Game.hpp"
+// 嗯，强行简洁
+// 虽然这些类做出来也不是拿来用的
+// 但是还是勉强做成这个样子
+// 可以算是强迫症的夙愿了吧
+// 完结撒花(?)
+int main()
+{
+    Game my_game;
+    my_game.start();
+    return 0;
+}
